289302b3b4f4e2ca8a5bc2b2532f5c2f3ccfd4e1
Vladimir Zotov <zott14@outlook.com>
2018-03-27 14:07:30 +0300

    Add right  and isosceles triangles, also rhombus.

--- rectangleByStars/Program.cs
+++ rectangleByStars/Program.cs
@@ -10,15 +10,20 @@ namespace rectangleByStars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число (ширина прямоугольника): ");
-            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите число (высота прямоугольника): ");
-            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число: ");
+            int y = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < height; i++)
+            int z = 1;
+            int buf = y;
+
+            // прямоугольник
+
+            for (int i = 0; i < x; i++)
             {
-                for (int j = 0; j < width; j++)
+                for (int j = 0; j < y; j++)
                 {
                     Console.Write("*");
                 }
@@ -26,6 +31,81 @@ namespace rectangleByStars
                 Console.WriteLine();
             }
 
+            Console.WriteLine();
+
+            //Прямоугольный треугольник
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            //Равнобедренный треугольник
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < z; k++)
+                {
+                    Console.Write("*");
+                }
+
+                y -= 1;
+                z += 2; 
+
+                Console.WriteLine();
+            }
+
+            z = 1;
+
+            Console.WriteLine();
+
+            //Ромб
+
+            for (int i = 0; i < x+ x + 1; i++)
+            {
+                if (i < x)
+                {
+                    for (int j = 0; j < buf; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 0; k < z; k++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    buf -= 1;
+                    z += 2;
+                }
+                else
+                {
+                    for (int j = 0; j < buf; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 0; k < z; k++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    buf += 1;
+                    z -= 2;
+                }
+                Console.WriteLine();
+            }
+
+
+            // Delay
             Console.ReadKey();
         }
     }
